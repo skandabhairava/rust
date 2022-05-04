afabe7366a704b01e9cd3084380c41f4bf6cced1
skandabhairava <skandabhairava@gmail.com>
2022-05-04 19:23:01 +0530

    updated mini-grep with iterators

--- mini_grep/src/lib.rs
+++ mini_grep/src/lib.rs
@@ -26,14 +26,16 @@ pub struct Config {
 }
 
     impl Config{
-        pub fn new(args: &[String]) -> Result<Config, &str> {
-
-            if args.len() < 3 {
-                return Err("Not enough arguments!");
-            }
-
-            let query = args[1].clone();
-            let filename = args[2].clone();
+        pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+            args.next();
+            let query = match args.next() {
+                Some(arg) => arg,
+                None => return Err("Didn't get a query string"),
+            };
+            let filename = match args.next() {
+                Some(arg) => arg,
+                None => return Err("Didn't get a filename"),
+            };
 
             let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -43,15 +45,10 @@ pub struct Config {
 //////////////////////////////////////////////////////////////
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query){
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
